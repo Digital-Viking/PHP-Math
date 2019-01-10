e3c43e63147be8f5c6722038bc26e91d12f516a0
David Weston Webb <westwebb@gmail.com>
2019-01-10 10:05:18 -0500

    Create math.php

---
+++ math.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+echo 56 + 45;
+// 101;
+echo "<br>";
+
+echo 56 - 45;
+// 11;
+echo "<br>";
+
+echo 56 * 45;
+// 2520;
+echo "<br>";
+
+echo 56 / 45;
+// 1.2444444444444;
+
+echo "<br>";
+echo "<br>";
+
+echo 45 + 34 + 45 + / 421 - 45;
+// 3.6342042...;
+echo "<br>";
+
+echo 5 + 5 * 10;
+// 55;
+echo "<br>";
+
+echo (5 + 5) * 10;
+// 100;
+
+echo "<br>";
+echo "<br>";
+
+$number1 = 12;
+$number2 = 24;
+
+echo $number1 * $number2;
+// 288;
+
+?>
+
+</body>
